7a74d19d5a0801929ccf2208b7b869d44dfc6b9b
Roman Kovalchuk <37290102+hactor171@users.noreply.github.com>
2020-08-24 21:45:02 +0200

    Adding image loader

--- app/src/main/java/com/example/equationsolverapp/MainActivity.java
+++ app/src/main/java/com/example/equationsolverapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.equationsolverapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
@@ -21,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
     ImageView equationImage;
     TextView detectedText;
     Button detectTextOnImage;
+    Button loadImage;
+
+    private static final int Perm_Code = 1001;
+    private static final int Image_Code = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         equationImage = (ImageView) findViewById(R.id.imageView);
         detectedText = (TextView) findViewById(R.id.textView);
         detectTextOnImage = (Button) findViewById(R.id.button);
+        loadImage = (Button) findViewById(R.id.load_button);
 
         detectTextOnImage.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -38,8 +50,43 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        loadImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                CheckPermission();
+
+            }
+        });
     }
 
+    public void CheckPermission()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            {
+                String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                requestPermissions(permissions, Perm_Code);
+            }
+            else
+            {
+                //permission granted
+                loadImageFromGallery();
+            }
+        }
+        else
+        {
+            loadImageFromGallery();
+        }
+    }
+
+    public void loadImageFromGallery()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, Image_Code);
+    }
 
     public void RecognizeText() {
 
@@ -61,11 +108,35 @@ public class MainActivity extends AppCompatActivity {
             String line = textBlock.getValue();
 
             stringBuilder.append(line);
-            System.out.println(line);
-
         }
 
         detectedText.setText(stringBuilder);
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Image_Code && resultCode == RESULT_OK) {
+            equationImage.setImageURI(data.getData());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case Perm_Code:
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    loadImageFromGallery();
+                }
+                else
+                {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
